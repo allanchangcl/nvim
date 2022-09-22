ad5957196177af5b026eb923183b4c8967c8f8cb
Chang CL <chang@cheeloy.com>
2022-09-22 13:43:30 +0800

    try get inlay hints to work - not working

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,19 +1,38 @@
-local bin_name = 'pyright-langserver'
-local cmd = { bin_name, '--stdio' }
+local util = require("lspconfig.util")
+
+local bin_name = "pyright-langserver"
+local cmd = { bin_name, "--stdio" }
+
+local root_files = {
+	"pyproject.toml",
+	"setup.py",
+	"setup.cfg",
+	"requirements.txt",
+	"Pipfile",
+	"pyrightconfig.json",
+}
 
 return {
---   cmd = { "py" },
-  cmd = cmd,
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "basic",
-        diagnosticMode = "workspace",
-        inlayHints = {
-          variableTypes = true,
-          functionReturnTypes = true,
-        },
-      },
-    },
-  },
+	default_config = {
+		cmd = cmd,
+		filetypes = { "python" },
+		root_dir = util.root_pattern(unpack(root_files)),
+		single_file_support = true,
+		settings = {
+			python = {
+				analysis = {
+					autoSearchPaths = true,
+					useLibraryCodeForTypes = true,
+					diagnosticMode = "workspace",
+				},
+			},
+		},
+	},
+	docs = {
+		description = [[
+https://github.com/microsoft/pyright
+
+`pyright`, a static type checker and language server for python
+]],
+	},
 }
